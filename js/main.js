101e0181c286ca9c1baaa861a35e03aa7a842848
Adorjan Daczo <adorjand@users.noreply.github.com>
2022-09-04 21:13:01 +0300

    - desktop layout - form functionality (validation, restrictions, input mask)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,140 @@
-console.log('Hello world!');
+import { VALIDATORS } from "./validators.js";
+import { RESTRICTIONS } from "./restrictions.js";
+
+function initRestrictions(fields) {
+    fields.forEach((field) => {
+        let fieldElement = document.querySelector(field.selector);
+
+        let restrictions = field.restrictions;
+
+        if (restrictions) {
+            restrictions.forEach((restriction) => {
+                if (Array.isArray(restriction)) {
+                    let fn = restriction[0];
+                    let param = restriction[1];
+
+                    fn(fieldElement, param);
+                } else {
+                    restriction(fieldElement);
+                }
+            });
+        }
+    });
+}
+
+const FIELDS = [
+    {
+        selector: "#cardholderName",
+        validators: [VALIDATORS.notBlank, VALIDATORS.name],
+    },
+    {
+        selector: "#cardNumber",
+        validators: [VALIDATORS.notBlank, VALIDATORS.numbersOnly, [VALIDATORS.exactNumbersLength, 16]],
+        restrictions: [RESTRICTIONS.cardNumberMask],
+    },
+    {
+        selector: "#expMonth",
+        validators: [VALIDATORS.notBlank, VALIDATORS.numbersOnly, VALIDATORS.month],
+        restrictions: [[RESTRICTIONS.maxLength, 2]],
+    },
+    {
+        selector: "#expYear",
+        validators: [VALIDATORS.notBlank, VALIDATORS.numbersOnly],
+        restrictions: [[RESTRICTIONS.maxLength, 2]],
+    },
+    {
+        selector: "#cvc",
+        validators: [VALIDATORS.notBlank, VALIDATORS.numbersOnly, [VALIDATORS.exactNumbersLength, 3]],
+        restrictions: [[RESTRICTIONS.maxLength, 3]],
+    },
+];
+
+// Iterate through each field and validate them
+function validate(fields) {
+    let hasErrors = false;
+
+    fields.forEach((field) => {
+        if (!isValidField(field.selector, field.validators)) {
+            hasErrors = true;
+        }
+    });
+
+    return !hasErrors;
+}
+
+// Run the validators defined for each field
+function isValidField(selector, validators) {
+    let hasError = false;
+    let fieldElement = document.querySelector(selector);
+
+    // If the field was not found -> nothing to validate, consider as valid
+    if (!fieldElement) {
+        console.warn(`The '${selector}' field was not found, check for typos in the selector name.`);
+        return true;
+    }
+
+    // No validators defined -> nothing to validate, consider as valid
+    if (validators === undefined || !validators.length) {
+        console.warn(`No validators defined for the '${selector}' field.`);
+        return true;
+    }
+
+    validators.forEach((validator) => {
+        if (!hasError && !isValid(fieldElement.value, validator)) {
+            let errorMessage = validator.errorMessage;
+
+            if (Array.isArray(validator)) {
+                errorMessage = validator[0].errorMessage.replace("___variable___", validator[1]);
+            }
+
+            showError(fieldElement, errorMessage);
+            hasError = true;
+        }
+    });
+
+    return !hasError;
+}
+
+function isValid(value, validator) {
+    if (Array.isArray(validator)) {
+        return validator[0].validatorFunction(value, validator[1]);
+    }
+
+    return validator.validatorFunction(value);
+}
+
+function showError(field, message) {
+    let placeholder = field.getAttribute("data-project-error");
+
+    let errorMessage = document.createElement("span");
+    errorMessage.classList.add("error-message");
+    errorMessage.innerText = message;
+
+    field.classList.add("has-error");
+
+    if (!placeholder) {
+        field.parentNode.insertBefore(errorMessage, field.nextSibling);
+        return;
+    }
+
+    let placeholderElement = document.querySelector(placeholder);
+
+    if (placeholderElement && placeholderElement.childNodes.length === 0) {
+        placeholderElement.appendChild(errorMessage);
+    }
+}
+
+function resetErrors() {
+    // Remove error state from fields
+    document.querySelectorAll(".has-error").forEach((element) => element.classList.remove("has-error"));
+    // Remove error messages
+    document.querySelectorAll(".error-message").forEach((element) => element.remove());
+}
+
+initRestrictions(FIELDS);
+
+document.querySelector("#cardForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    resetErrors();
+    console.log(validate(FIELDS));
+});
---
+++ js/validators.js
@@ -0,0 +1,48 @@
+export const VALIDATORS = {
+    notBlank: {
+        validatorFunction: function (value) {
+            return value !== undefined && value !== "";
+        },
+        errorMessage: "Can't be blank",
+    },
+    name: {
+        validatorFunction: function (value) {
+            return /^\s*([A-Za-z]+([.] |[-']| )?)+[A-Za-z]+\.?\s*$/.test(value);
+        },
+        errorMessage:
+            "Invalid characters, only letters without diacritics, spaces, periods (.), apostrophes (') and hyphens (-) are allowed",
+    },
+    numbersOnly: {
+        validatorFunction: function (value) {
+            // Trim spaces between numbers (Ex. card number mask)
+            value = value.replace(/ /g, "");
+
+            return /^\d+$/.test(value);
+        },
+        errorMessage: "Wrong format, numbers only",
+    },
+    exactNumbersLength: {
+        validatorFunction: function (value, expectedLength) {
+            // No validator param defined -> can't validate, consider as valid
+            if (expectedLength === undefined) {
+                console.warn("Missing validator param for expected length.");
+                return true;
+            }
+
+            // Trim spaces between numbers (Ex. card number mask)
+            value = value.replace(/ /g, "");
+
+            return value.length === expectedLength;
+        },
+        errorMessage: "This field must be exactly ___variable___ number(s) long",
+    },
+    month: {
+        validatorFunction: function (value) {
+            if (value.charAt(0) === "0") {
+                value = value.substring(1);
+            }
+            return value < 13;
+        },
+        errorMessage: "Invalid month",
+    },
+};
